70a0b3bac00d09968e3288f8b66adcdfb8ad99dc
Jmsierra3101 <josemiguelsierra650@gmail.com>
2024-05-14 08:15:11 -0500

    ejercicios flor

---
+++ dartADSO2873711/Ciclos/ejerciciosGuia4/FOR/ejercicioFOR_02.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+//Jose Miguel Sierra -ejercicio 02
+
+void main() {
+  List<int> numeros = [];
+  int numero, contador = 0;
+
+  for (int i = 0; i < 10; i++) {
+    print("Ingrese el numero ${contador + 1}");
+    numero = int.parse(stdin.readLineSync()!);
+    numeros.add(numero);
+    contador++;
+  }
+  print("Los numeros positivos son:");
+
+  for (int numero in numeros) {
+    if (numero > 0) {
+      print("$numero");
+    }
+  }
+}
---
+++ dartADSO2873711/Ciclos/ejerciciosGuia4/FOR/ejercicioFOR_05.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+//Jose Miguel Sierra -ejercicio 05
+
+void main(List<String> args) {
+  int numero, producto;
+
+  print("Ingrese el numero del cual desea saber la tabla de multiplicar");
+  numero = int.parse(stdin.readLineSync()!);
+
+  print("Tabla de multiplicar (num $numero)");
+  for (int i = 0; i <= 10; i++) {
+    producto = numero * i;
+    print("$numero X $i = $producto");
+  }
+}
